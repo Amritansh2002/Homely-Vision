7ea2437ca6be9761d0715972074f7fb584d35970
srivastavaamritansh02@gmail.com <bit21cs41bit.ac.in>
2024-09-17 00:34:07 +0530

    add

---
+++ save.php
@@ -0,0 +1,8 @@
+<?php
+$name=$_name;
+$email=$_email;
+$subject=$_subject;
+$message=$_message;
+echo'this is working';
+
+?>
---
+++ script.js
@@ -0,0 +1,8 @@
+const bar= document.getElementById('bar');
+const nav=document.getElementById('navbar');
+
+if(ba){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+}
